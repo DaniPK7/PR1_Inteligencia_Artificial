9ec3333a233f5581a9901898de88b0a3e441cbd4
al375523 <al375523@uji.es>
2020-11-03 11:19:44 +0100

    Pathfinding added

---
+++ Assets/Scripts/A Star/Grid.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public Transform startPosition;
+    public LayerMask wallMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    public float Distance;
+
+    Node[,] grid;
+    public List<Node> finalPath;
+
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    private void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 bottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+        for (int j = 0; j < gridSizeX; j++)
+        {
+            for (int i = 0; i < gridSizeX; i++)
+            {
+                Vector3 worldPoint = bottomLeft + Vector3.right * (i * nodeDiameter + nodeRadius) + Vector3.forward * (j * nodeDiameter + nodeRadius);
+                bool wall = true;
+
+                if (Physics.CheckSphere(worldPoint, nodeRadius, wallMask))
+                {
+                    wall = false;
+                }
+                grid[i, j] = new Node(wall, worldPoint, i, j);
+            }
+        }
+    }
+
+    public Node NodeFromWorldPosition(Vector3 worldPosition)
+    {
+        float xpoint = ((worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x);
+        float ypoint = ((worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y);
+
+        xpoint = Mathf.Clamp01(xpoint);
+        ypoint = Mathf.Clamp01(ypoint);
+
+        int x = Mathf.RoundToInt((gridSizeX - 1) * xpoint);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * ypoint);
+
+        return grid[x, y];
+    }
+
+    public List<Node> GetNeighboringNodes(Node a_Node)
+    {
+        List<Node> neighboringNodes = new List<Node>();
+        int xCheck;
+        int yCheck;
+
+        xCheck = a_Node.gridX + 1;
+        yCheck = a_Node.gridY;
+        if(xCheck>=0 && xCheck < gridSizeX)
+        {
+            if (yCheck >= 0 && yCheck < gridSizeY)
+            {
+                neighboringNodes.Add(grid[xCheck, yCheck]);
+            }
+        }
+
+        xCheck = a_Node.gridX - 1;
+        yCheck = a_Node.gridY;
+        if (xCheck >= 0 && xCheck < gridSizeX)
+        {
+            if (yCheck >= 0 && yCheck < gridSizeY)
+            {
+                neighboringNodes.Add(grid[xCheck, yCheck]);
+            }
+        }
+
+        xCheck = a_Node.gridX;
+        yCheck = a_Node.gridY + 1;
+        if (xCheck >= 0 && xCheck < gridSizeX)
+        {
+            if (yCheck >= 0 && yCheck < gridSizeY)
+            {
+                neighboringNodes.Add(grid[xCheck, yCheck]);
+            }
+        }
+
+        xCheck = a_Node.gridX;
+        yCheck = a_Node.gridY-1;
+        if (xCheck >= 0 && xCheck < gridSizeX)
+        {
+            if (yCheck >= 0 && yCheck < gridSizeY)
+            {
+                neighboringNodes.Add(grid[xCheck, yCheck]);
+            }
+        }
+        /*
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                //if we are on the node tha was passed in, skip this iteration.
+                if (x == 0 && y == 0)
+                {
+                    continue;
+                }
+
+                xCheck = a_Node.gridX + x;
+                yCheck = a_Node.gridY + y;
+
+                //Make sure the node is within the grid.
+                if (xCheck >= 0 && xCheck < gridSizeX && yCheck >= 0 && yCheck < gridSizeY)
+                {
+                    neighboringNodes.Add(grid[xCheck, yCheck]); //Adds to the neighbours list.
+                }
+
+            }
+        }
+        */
+
+        return neighboringNodes;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x,10, gridWorldSize.y));
+
+        if (grid != null) { 
+            foreach(Node node in grid)
+            {
+                if (node.isWall)
+                {
+                    Gizmos.color = Color.white;
+                }
+                else
+                {
+                    Gizmos.color = Color.yellow;
+                }
+                if (finalPath != null && finalPath.Contains(node))
+                {
+                    Gizmos.color = Color.red;
+                    Debug.Log("Ha llegado chacho");
+                }
+                
+                Gizmos.DrawCube(node.position, Vector3.one * (nodeDiameter - Distance));
+            }
+        }
+            
+    }
+}
---
+++ Assets/Scripts/A Star/Pathfinding.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    Grid grid;
+    public GameObject gameManager;
+    public Transform startPosition;
+    public Transform targetPosition;
+
+    private void Awake()
+    {
+        grid =gameManager.GetComponent<Grid>();
+    }
+    private void Update()
+    {
+        FindPath(startPosition.position, targetPosition.position);
+    }
+
+    private void FindPath(Vector3 a_startPosition, Vector3 a_targetPosition)
+    {
+        Node StartNode = grid.NodeFromWorldPosition(a_startPosition);
+        Node TargetNode = grid.NodeFromWorldPosition(a_targetPosition);
+
+        List<Node> OpenList = new List<Node>();
+        HashSet<Node> ClosedList = new HashSet<Node>();
+
+        OpenList.Add(StartNode);
+
+        while (OpenList.Count > 0)
+        {
+            Node currentNode = OpenList[0];
+            for (int i = 1; i < OpenList.Count; i++)
+            {
+                if(OpenList[i].FCost<currentNode.FCost || OpenList[i].FCost == currentNode.FCost && OpenList[i].hCost<currentNode.hCost)
+                {
+                    currentNode = OpenList[i];
+                }
+            }
+            OpenList.Remove(currentNode);
+            ClosedList.Add(currentNode);
+
+            if (currentNode == TargetNode)
+            {
+                GetFinalPath(StartNode, TargetNode);
+                break;
+            }
+
+            foreach(Node neighborNode in grid.GetNeighboringNodes(currentNode))
+            {
+                if(!neighborNode.isWall || ClosedList.Contains(neighborNode))
+                {
+                    continue;
+                }
+
+                int MoveCost = currentNode.gCost + GetManhattenDistance(currentNode, neighborNode);
+
+                if (!OpenList.Contains(neighborNode) || MoveCost < neighborNode.FCost)
+                {
+                    neighborNode.gCost = MoveCost;
+                    neighborNode.hCost = GetManhattenDistance(neighborNode, TargetNode);
+                    neighborNode.parent = currentNode;
+
+                    if (!OpenList.Contains(neighborNode))
+                    {
+                        OpenList.Add(neighborNode);
+                    }
+                }
+            }
+        }
+    }
+
+    private void GetFinalPath(Node a_startingNode, Node a_EndNode)
+    {
+        List<Node> FinalPath = new List<Node>();
+        Node currentNode = a_EndNode;
+
+        while (currentNode!=a_startingNode)
+        {
+            FinalPath.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        FinalPath.Reverse();
+        grid.finalPath = FinalPath;
+    }
+
+    private int GetManhattenDistance(Node nodeA, Node nodeB)
+    {
+        int ix = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int iy = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        return ix + iy;
+    }
+}
