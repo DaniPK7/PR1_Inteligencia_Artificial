0e959a3cc6af63509f92043d6a33257b13ef619b
al375523 <al375523@uji.es>
2020-11-04 17:12:15 +0100

    Inicio de movimiento Pathfinding hacia el jugador

--- Assets/Scripts/A Star/Grid.cs
+++ Assets/Scripts/A Star/Grid.cs
@@ -16,7 +16,7 @@ public class Grid : MonoBehaviour
     float nodeDiameter;
     int gridSizeX, gridSizeY;
 
-    private void Start()
+    public void Awake()
     {
         nodeDiameter = nodeRadius * 2;
         gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -5,37 +5,32 @@ using UnityEngine.AI;
 
 public class WaypointPatrol : MonoBehaviour
 {
-    public NavMeshAgent navMeshAgent;
-    public Transform[] waypoints;
+    public Vector3 startpoint;
 
     public GameObject Player;
 
-    int m_CurrentWaypointIndex;
+    public Grid grid;
 
     public bool chasePlayer;
 
     void Start()
     {
-        navMeshAgent.SetDestination(waypoints[0].position);
+        startpoint=GetComponent<Transform>().position;
         chasePlayer = false;
+        
     }
 
     void Update()
     {
         if (chasePlayer)
         {
-            navMeshAgent.SetDestination(Player.transform.position);
+            grid.Awake();
         }
 
         else
         {
-            if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
-            {
-                m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
-                navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
-            }
+            
         }
-
     }
     private void OnTriggerStay(Collider other)
     {
@@ -44,8 +39,6 @@ public class WaypointPatrol : MonoBehaviour
             print("Dentro de rango");
             chasePlayer = true;
         }
-
-
     }
 
     private void OnTriggerExit(Collider other)
